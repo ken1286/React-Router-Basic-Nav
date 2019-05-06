64e8e399a32c36c410463cd596b85652f6519198
ken1286 <kindle1286@gmail.com>
2019-05-06 11:45:26 -0700

    first push

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { Home, About, Contact, Navigation } from './components';
 
 const App = () => (
+  <Router>
   <div>
     <Navigation />
   </div>
+  </Router>
 );
 
 export default App;
